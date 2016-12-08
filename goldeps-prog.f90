5f810779846fe293d1536ddfc1867a5b773366c9
Daria Shatko <dasha.shatco@gmail.com>
2016-12-08 16:17:33 +0400

    08/12/2016 - changed all frequencies in eV, improved gold-module, deleted third integral in Kramers-Kronig procedure

--- goldeps-prog.f90
+++ goldeps-prog.f90
@@ -1,94 +1,90 @@
 program goldeps
 
-use silicon
 use gold
-use dopcasimir
-use spline
-use silicon
-use quadpack
 
 implicit none
 
+real, parameter:: pi=3.14159265359
+real(8), parameter :: kb=8.617e-5  ! eV/K kb=1.38e-23 J/K  Boltzman constant
+real(8), parameter :: h=6.585e-16  ! eV*s h=1.054e-34J*s  Plank constant
+
 integer,parameter:: n1=310 !rows number in file
-!Palik data table
-real(8):: matrixAu(n1,3) , x(100), y1(100), y2(100)
-!permettivity vector
-real(8), allocatable:: epsAur(:)
+integer,parameter:: nw=100 !number of Matsubara frequencies
+
+real(8):: matrixAu(n1,3) , x(nw), eps_Drude(nw), eps_Mar(nw), eps_Gen(nw), epsAur(nw)
 !counters
 integer:: i,k,j
 !integration results
-real(8):: integralA1,integralA2,integralA3
+real(8):: integralA1,integralA2
 !variables needed fo Kr-Kr integral in cspint subroutine
 real(8)::funvalAu(n1),SpcoefA(3,n1), exintA(n1), workA(n1)
 !variables for Kr-K integral in qags subroutine
-real(4):: abserrA,abserrA2
-integer(4)::nevalA,ierA,nevalA2,ierA2
+integer(4)::nevalA,ierA
+real(8):: eV,T
 
-
+print*, "Process..."
 open(unit=15, file='resultAu.txt', status='old')
 !read matrix from file
 !1st column - frequencies
 !2nd column - real part of permittivity
 !3rd column - imaginary part of permittivity
 
+open(unit=16, file='resultAu_eV.txt', status='replace')
+
+eV=1.519e15 !rad/s
+200 format(3(f15.3))
+
 do i=1,n1
 read(15,*), (matrixAu(n1+1-i,j),j=1,3)
 end do
 
-200 format(' ', e15.3, f15.7, f15.7)
-
-!do i=1,n1
-!write(*,200), (matrixAu(i,j),j=1,3)
-!end do
+do i=1,n1
+matrixAu(i,1)=matrixAu(i,1)/eV
+write(16,200), (matrixAu(i,j),j=1,3)
+end do
 
 !Kramers-Kronig for 'freq'-vector
+T=200._8
 
-allocate(epsAur(100))
-freqA=20._8
-
-	do i=1,100
+	do i=1,nw
 	
+	x(i)=2*pi*kb*T*i/h
+    x(i)=x(i)/eV
+    freqA=x(i)
+
 	!integrates the Drude-model approximation
 	call qnc79(drude_to_int, 0.0_8, matrixAu(1,1), 1.0e-3_8, integralA1, ierA, nevalA)
-	PRINT*, 'INTEGRAL1=', integralA1
-	!print*, 'ier=', ierA
 
-	
 	!builds vector to integrate from the Palik data
 		do k=1,n1
 		funvalAu(k)=matrixAu(k,3)*matrixAu(k,1)/(matrixAu(k,1)**2+freqA**2)
 		end do
 	!integrates the vector of data
  	call cspint(n1, matrixAu(1:n1,1), funvalAu, matrixAu(1,1), matrixAu(n1,1), SpcoefA, exintA, workA, integralA2)
-	print*, 'integral2=', integralA2
 
-	!integrates the oscillator-function
-	call monte_carlo(oscil_to_int, matrixAu(n1,1), matrixAu(n1,1)*1.0e4_8, 1000000, integralA3)
-	print*, 'integral3=', integralA3
-	!print*, 'ier3=', ierA2
-
-	
 	!Kr-Kr formula for permittivity 
-	epsAur(i)=(integralA1+integralA2+integralA3)*2/pi+1
-
-	x(i)=freqA
-	freqA=freqA*10**(0.2_8)
+	epsAur(i)=(integralA1+integralA2)*2/pi+1
 
 	end do
 
-open(unit=18, file='golddata1.txt', status='replace')
+open(unit=18, file='golddata.txt', status='replace')
 
-do i=1,100
-y1(i)=Drude(x(i),parAproxIm)
-y2(i)=epsMar(x(i))
+do i=1,nw
+eps_Drude(i)=Drude(x(i),parAproxIm)
+eps_Mar(i)=epsMar(x(i))
+eps_Gen(i)=Gen_Plasma(x(i))
 end do
 
-do i=1,100
-write(18,*) x(i), epsAur(i), y1(i), y2(i)
+100 format(5(f15.3))
+
+do i=1,nw
+write(18,100) x(i), epsAur(i), eps_Drude(i), eps_Mar(i), eps_Gen(i)
 end do
 
-deallocate(epsAur)
 close(15)
+close(16)
 close(18)
+
+print*, "Done!"
 
 end program
