7d0c72022cb23989ecb4abeac83ed14380297a4c
Dasha <daria.satco@gmail.com>
2017-02-19 19:31:52 +0000

    19.02.17 - added Brendel-Bormann and Lorentz-Drude models

--- gold.f90
+++ gold.f90
@@ -1,25 +1,49 @@
 module gold
 
+interface
+    SUBROUTINE WOFZ (XI, YI, U, V, FLAG)
+    real(8):: xi, yi, u,v
+    logical::flag
+    end subroutine
+end interface
+
 real(8), public:: freqA
 !parameters (Drude model)
 !vector format: (wp, damp)
 !Mostepanenko
 real(8), dimension(2), parameter:: parMost=(/9.0, 0.035/) !eV
+!Rakic
+real(8), dimension(2), parameter:: parRakic=(/7.87, 0.053/) !eV
 !approximation data of the imaginary part (Palik)
 real(8), dimension(2), parameter:: parAproxIm=(/6.62, 0.089/) !eV
 !approximation data of the real part (Palik)
 real(8), dimension(2), parameter:: parAproxRe=(/7.93, 0.08/) !eV
 
 !oscillator model, Mostepanenko parameters
-real(8), dimension(6), parameter:: gAu=(/7.091, 41.46, 2.7, 154.7, 44.55, 309.6/) !*(1.519e15)**2
-real(8), dimension(6), parameter:: gammaAu=(/0.75, 1.85, 1.0, 7.0, 6.0, 9.0/) !*1.519e15
-real(8), dimension(6), parameter:: wAu=(/3.05, 4.15, 5.4, 8.5, 13.5, 21.5/) !*1.519e15
+real(8), dimension(6), parameter:: gAu=(/7.091, 41.46, 2.7, 154.7, 44.55, 309.6/) !eV
+real(8), dimension(6), parameter:: gammaAu=(/0.75, 1.85, 1.0, 7.0, 6.0, 9.0/) !eV
+real(8), dimension(6), parameter:: wAu=(/3.05, 4.15, 5.4, 8.5, 13.5, 21.5/) !eV
+
+!Lorentz-Drude model, Rakic parameters
+real(8), dimension(6), parameter:: fR=(/0.024, 0.01, 0.071, 0.601, 4.384, 0.0/) !eV
+! gR=fR*parRakic(1)**2
+real(8), dimension(6), parameter:: gammaR=(/0.241, 0.345, 0.87, 2.494, 2.214, 0.0/) !eV
+real(8), dimension(6), parameter:: wR=(/0.415, 0.83, 2.969, 4.304, 13.32, 0.0/) !eV
+
+!Brendel-Bormann model, Rakic parameters
+real(8), dimension(6), parameter:: fRb=(/0.054, 0.05, 0.312, 0.719, 1.648, 0.0/)
+! gRb=fRb*parRakic(1)**2
+real(8), dimension(6), parameter:: gammaRb=(/0.074, 0.035, 0.083, 0.125, 0.179, 0.0/)
+real(8), dimension(6), parameter:: wRb=(/0.218, 2.885, 4.069, 6.137, 27.97, 0.0/)
+real(8), dimension(6), parameter:: sigmaRb=(/0.742, 0.349, 0.830, 1.246, 1.795, 1.0/)
+
 
 contains
 
 function Drude(x, param)
 !Drude-model permettivity function eps(iw)
 !param - vector of parameters (wp, damp)
+
 real(8):: x, Drude, param(2)
 
 	Drude=1+param(1)**2/(x**2+param(2)*x);
@@ -55,29 +79,72 @@ end function
 
 !---------------------------------------------------
 
-function oscil_gold(x)
-!imaginary part of oscillator approximation
+function oscil_gold_im(x,a,b,c)
+!imaginary part of oscillator approximation (Mostepanenko/Racik/other...)
 
-real(8):: oscil_gold, x
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+
+real(8):: oscil_gold_im, x, a(6), b(6), c(6)
+
+oscil_gold_im=0.0_8
+
+do i=1,size(a)
+oscil_gold_im=oscil_gold_im-a(i)*b(i)*x/((c(i)**2-x**2)**2+(b(i)*x)**2)
+end do
+
+end function
+!--------------------------------------------------
+
+function oscil_gold_re(x,a,b,c)
+!real part of oscillator approximation (Mostepanenko/Racik/other...)
+
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+
+real(8):: oscil_gold_re, x, a(6), b(6), c(6)
+
+oscil_gold_re=0.0_8
+
+do i=1,size(a)
+oscil_gold_re=oscil_gold_re+a(i)*(c(i)**2-x**2)/((c(i)**2-x**2)**2+(b(i)*x)**2)
+end do
+
+end function
+!--------------------------------------------------
+
+function oscil_gold(x,a,b,c)
+!Lorentz function (oscillators) eps(iw)
+
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+
+real(8):: oscil_gold, x, a(6), b(6), c(6)
 
 oscil_gold=0.0_8
 
-do i=1,6
-oscil_gold=oscil_gold+gAu(i)*gammaAu(i)*x/((wAu(i)**2-x**2)**2+(gammaAu(i)*x)**2)
+do i=1,size(a)
+oscil_gold=oscil_gold+a(i)/(c(i)**2+x**2-b(i)*x)
 end do
 
 end function
 
 !--------------------------------------------------
 
-function oscil_to_int(x)
-!KK expression under integral fo oscillators
-
-real(8):: x, oscil_to_int
-
-oscil_to_int=x/(x**2+freqA**2)*oscil_gold(x)
-
-end function
+!function oscil_to_int(x)
+!!KK expression under integral for oscillators
+!
+!real(8):: x, oscil_to_int
+!
+!oscil_to_int=x/(x**2+freqA**2)*oscil_gold(x)
+!
+!end function
 
 !--------------------------------------------------
 
@@ -86,7 +153,7 @@ function drude_to_int(x)
 
 real(8):: x, drude_to_int, param(2)
 
-param=parAproxRe
+param=parAproxIm
 drude_to_int=param(1)**2*param(2)/((x**2+param(2)**2)*(x**2+freqA**2))
 
 end function
@@ -94,20 +161,109 @@ end function
 
 !-------------------------------------------------
 
-!Generalized plasma-like dielectric permettivity function
-
-function Gen_Plasma(x)
+function Gen_Plasma(x,a,b,c)
+!Generalized plasma-like dielectric permettivity function eps(iw)
 
 real(8):: x, Gen_Plasma
+real(8):: a(6), b(6), c(6)
 
-Gen_Plasma=1._8+(parMost(1)/x)**2
+Gen_Plasma=1._8+(parMost(1)/x)**2+oscil_gold(x,a,-b,c)
 
-do i=1,6
-Gen_Plasma=Gen_Plasma+gAu(i)/(wAu(i)**2+x**2-gammaAu(i)*x)
+end function
+
+!-------------------------------------------------
+
+function Lorentz_Drude(x,a,b,c)
+!Lorentz-Drude model (Rakic) eps(iw)
+
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+
+real(8):: x, Lorentz_Drude, a(6), b(6), c(6)
+
+Lorentz_Drude=Drude(x, parRakic)+oscil_gold(x,a,b,c)
+
+end function
+
+!-------------------------------------------------
+
+function Brendel_Bormann_oscil(x,a,b,c,d)
+!Brendel-Bormann model, oscillation part in eps(iw)
+
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+!d - vector of sigma (dispersion)
+
+real, parameter:: pi=3.14159265359
+
+real(8):: x, Brendel_Bormann_oscil, a(6),b(6),c(6),d(6)
+real(8):: WvalueRe1, WvalueIm1, WvalueRe2, WvalueIm2
+logical:: logvar
+real(8):: Re,Im, const
+!argW=Re+i*Im
+
+Brendel_Bormann_oscil=0.0D+00
+
+do i=1,size(a)
+
+!calculate argument
+Re=c(i)/(sqrt(2.0)*d(i))
+Im=sqrt(x**2+x*b(i))/(sqrt(2.0)*d(i))
+!call Faddeeva function w(z)
+call wofz(Re, Im, WvalueRe1, WvalueIm1, logvar) !first summand
+call wofz(-Re, Im, WvalueRe2, WvalueIm2, logvar) !second summand
+
+
+!calculate prefactor
+const=sqrt(pi)*a(i)/(2*sqrt(2.0)*d(i)*sqrt(x**2+x*b(i)))
+
+if (logvar.eqv. .True.) then
+print*, 'Attention, error in wofz subroutine'
+else
+Brendel_Bormann_oscil=Brendel_Bormann_oscil+(WvalueRe1+WvalueRe2)*const
+end if
+
 end do
 
 end function
+
 !-------------------------------------------------
+function Brendel_Bormann(x,param,a,b,c,d)
+!Brendel-Bormann model, eps(iw)
+
+real(8):: Brendel_Bormann, x
+real(8):: a(6),b(6),c(6),d(6), param(2)
+
+!you can fix your own parameters
+!a - vector of oscillator strengths
+!b - vector of relaxation parameters
+!c - vector of resonant frequancies
+!d - vector of sigma (dispersion)
+
+Brendel_Bormann=Drude(x, param)+Brendel_Bormann_oscil(x, a, b, c, d)
+
+end function
+
+!-------------------------------------------------
+
+function epsRe(x)
+!real part of dielectric perm. function, Drude model
+
+real(8):: x, epsRe
+real(8):: param(2)
+
+param=parAproxRe
+
+epsRe=1._8-param(1)**2/(x**2+param(2)**2)
+
+end function
+
+
+!--------------------------------------------------
 
 
 end module
--- goldeps-prog.f90
+++ goldeps-prog.f90
@@ -9,18 +9,19 @@ real(8), parameter :: kb=8.617e-5  ! eV/K kb=1.38e-23 J/K  Boltzman constant
 real(8), parameter :: h=6.585e-16  ! eV*s h=1.054e-34J*s  Plank constant
 
 integer,parameter:: n1=310 !rows number in file
-integer,parameter:: nw=100 !number of Matsubara frequencies
+integer,parameter:: nw=500 !number of Matsubara frequencies
 
-real(8):: matrixAu(n1,3) , x(nw), eps_Drude(nw), eps_Mar(nw), eps_Gen(nw), epsAur(nw)
+real(8):: matrixAu(n1,3) , x(nw), eps_Drude(nw), eps_Mar(nw), eps_Gen(nw), epsAur(nw), eps_BB(nw)
 !counters
 integer:: i,k,j
 !integration results
-real(8):: integralA1,integralA2
+real(8):: integralA1,integralA2,integralA3
 !variables needed fo Kr-Kr integral in cspint subroutine
-real(8)::funvalAu(n1),SpcoefA(3,n1), exintA(n1), workA(n1)
+real(8)::funvalAu(n1),SpcoefA(3,n1), exintA(n1), workA(n1), funvalAu1(n1)
 !variables for Kr-K integral in qags subroutine
 integer(4)::nevalA,ierA
 real(8):: eV,T
+
 
 print*, "Process..."
 open(unit=15, file='resultAu.txt', status='old')
@@ -43,13 +44,14 @@ matrixAu(i,1)=matrixAu(i,1)/eV
 write(16,200), (matrixAu(i,j),j=1,3)
 end do
 
+T=300._8
 !Kramers-Kronig for 'freq'-vector
-T=200._8
 
 	do i=1,nw
 	
-	x(i)=2*pi*kb*T*i/h
-    x(i)=x(i)/eV
+!	x(i)=2*pi*kb*T*i/h
+!   x(i)=x(i)/eV
+    x(i)=0.2_8+0.01*(i-1)
     freqA=x(i)
 
 	!integrates the Drude-model approximation
@@ -72,13 +74,18 @@ open(unit=18, file='golddata.txt', status='replace')
 do i=1,nw
 eps_Drude(i)=Drude(x(i),parAproxIm)
 eps_Mar(i)=epsMar(x(i))
-eps_Gen(i)=Gen_Plasma(x(i))
+eps_Gen(i)=Gen_Plasma(x(i),gAu,gammaAu,wAu)
+eps_BB(i)=Brendel_Bormann(x(i), parRakic, fRb*parRakic(1)**2, gammaRb, wRb, sigmaRb)
 end do
 
-100 format(5(f15.3))
+100 format(7(f15.3))
+150 format(6(A15))
 
+!create the top of the table
+write(18,150) 'freq (eV)', 'K-K (Drude)', 'Drude', 'Marachevsky', 'Gen_plasma', 'Bren_Borm'
+!write values in the table
 do i=1,nw
-write(18,100) x(i), epsAur(i), eps_Drude(i), eps_Mar(i), eps_Gen(i)
+write(18,100) x(i), epsAur(i), eps_Drude(i), eps_Mar(i), eps_Gen(i), eps_BB(i)
 end do
 
 close(15)
